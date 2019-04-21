43591e1b95712d48fa2990ee14383440a3254ded
Devin Slick <devinslick@users.noreply.github.com>
2019-04-21 14:25:15 -0500

    Delete download_last_recording.py

--- python/download_last_recording.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import sys
-from ring_doorbell import Ring
-myring = Ring(sys.argv[1], sys.argv[2])
-doorbell = myring.doorbells[0]
-print(doorbell.recording_url(doorbell.last_recording_id))
-doorbell.recording_download(doorbell.history(limit=100)[0]['id'],filename='www/videos/last_recording.mp4',override=True)
-
