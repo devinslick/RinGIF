908ed916dfb369d0fe11dfe4d829fafa6e9386be
Devin Slick <devinslick@users.noreply.github.com>
2019-04-21 14:25:04 -0500

    Moving RinGIF to docker

--- python/check_last_recording.py
+++ python/check_last_recording.py
@@ -1,8 +1,3 @@
-#!/usr/bin/python
-import sys
-from ring_doorbell import Ring
-myring = Ring(sys.argv[1], sys.argv[2])
-doorbell = myring.doorbells[0]
-f= open("variables/recording.current","w+")
-f.write(doorbell.recording_url(doorbell.last_recording_id))
-f.close()
+mkdir -p /data
+touch /data/previous.recording
+python check_last_recording.py
