db4c46d66e9cb930bb08159d57164657d4a127a0
Devin Slick <devinslick@users.noreply.github.com>
2019-04-21 21:07:53 -0500

    Wait for long running job

--- RinGIF.sh
+++ RinGIF.sh
@@ -1,17 +1,19 @@
-mkdir -p /data
-touch /data/previous.recording
-results=$(python /check_last_recording.py)
-if [[ $results = *Updates* ]]
-then
-   echo "Download complete, beginning image extraction..."
-   /usr/bin/ffmpeg -i "/data/last_recording.mp4" -vf fps=$fps "/data/frame-%03d.jpg" -hide_banner
-   #optimize images
-   find /data/ -name "*.jpg" -type f -exec jpegtran -copy none -optimize -outfile {} {} \;
-   #downsize and convert to a GIF
-   echo "Building GIF..."
-   /usr/bin/convert -delay 20 -loop 0 -resize 192x108 -despeckle /data/*jpg /data/latest.gif
-   #delete the JPGs
-   rm -rf /data/*jpg
-   # optional feature to be added using the archivePath environmental variable
-   # /usr/bin/cp /data/images/latest.gif $archivePath/$(date +%Y%m%d%H%M%S).gif
+pythonRunning=$(ps -ef | grep python | wc -l)
+if [[ $pythonRunning == "1" ]]; then
+  mkdir -p /data
+  touch /data/previous.recording
+  results=$(python /check_last_recording.py)
+  if [[ $results = *Updates* ]]; then
+    echo "Download complete, beginning image extraction..."
+    /usr/bin/ffmpeg -i "/data/last_recording.mp4" -vf fps=$fps "/data/frame-%03d.jpg" -hide_banner
+    #optimize images
+    find /data/ -name "*.jpg" -type f -exec jpegtran -copy none -optimize -outfile {} {} \;
+    #downsize and convert to a GIF
+    echo "Building GIF..."
+    /usr/bin/convert -size 192x108 -despeckle delay 20 -loop 0 /data/*.jpg -resize 192x108 /data/latest.gif
+    #delete the JPGs
+    rm -rf /data/*jpg
+    # optional feature to be added using the archivePath environmental variable
+    # /usr/bin/cp /data/images/latest.gif $archivePath/$(date +%Y%m%d%H%M%S).gif
+  fi
 fi
