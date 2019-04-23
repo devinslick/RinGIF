3a062b692f758260ed20f8cec72e3e789f4a2fc7
Devin Slick <devinslick@users.noreply.github.com>
2019-04-22 19:05:30 -0500

    Update check_last_recording.py

--- check_last_recording.py
+++ check_last_recording.py
@@ -1,22 +1,17 @@
 from ring_doorbell import Ring
 import os
-import requests
-myring = ''
-new=''
-doorbell=''
 myring = Ring(os.environ['email'], os.environ['password'])
 doorbell = myring.doorbells[0]
 recordingID=str(doorbell.last_recording_id)
 with open('/data/previous.recording', 'r') as recording:
   old = str(recording.read())
+recording.close()
 if old != recordingID:
-  print("Updates")
-  r = requests.get(doorbell.recording_url(recordingID))
-  with open('/data/last_recording.mp4', 'wb') as f:
-    f.write(r.content)
-  f.close()
-  f= open("/data/previous.recording","w+")
-  f.write(str(recordingID))
-  f.close()
+  print("Updates needed")
+  doorbell = myring.doorbells[0]
+  doorbell.recording_download(doorbell.history(limit=100)[0]['id'],filename='/data/last_recording.mp4',override=True)
+  with open('/data/previous.recording', 'w') as tracker:
+    tracker.write(recordingID)
+  tracker.close()
 else:
   print("Nothing")
