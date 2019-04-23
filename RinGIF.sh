750fa0c4d44b609fb6d2ee5a02fbb63ae22a093f
Devin Slick <devinslick@users.noreply.github.com>
2019-04-22 21:23:54 -0500

    Update RinGIF.sh

--- RinGIF.sh
+++ RinGIF.sh
@@ -1,16 +1,15 @@
 pythonRunning=$(ps -ef | grep python | wc -l)
 if [[ $pythonRunning == "1" ]]; then
-  mkdir -p /data
   touch /data/previous.recording
   results=$(python /check_last_recording.py)
-  if [[ $results == *"Updates"* ]]; then
+  if [[ $results == "Updates" ]]; then
     echo "Download complete, beginning image extraction..."
     /usr/bin/ffmpeg -i "/data/last_recording.mp4" -vf fps=$fps "/data/frame-%03d.jpg" -hide_banner
     #optimize images
     find /data/ -name "*.jpg" -type f -exec jpegtran -copy none -optimize -outfile {} {} \;
     #downsize and convert to a GIF
     echo "Building GIF..."
-    /usr/bin/convert -size 192x108 -despeckle delay 20 -loop 0 /data/*.jpg -resize 192x108 /data/latest.gif
+    /usr/bin/convert -delay 20 -loop 0 /data/*.jpg -resize 192x108 /data/latest.gif
     #delete the JPGs
     rm -rf /data/*jpg
     # optional feature to be added using the archivePath environmental variable
