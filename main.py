b2272b92dd7f9d883cc8802dfc05bec855b8bb46
Devin Slick <github@devinslick.com>
2019-04-29 21:35:40 -0500

    Add timestamps to output for troubleshooting

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import csv
 import time
 import ffmpeg
 import wget
+import datetime
  
 email = str(os.environ['email'])
 password = str(os.environ['password'])
@@ -28,26 +29,24 @@ os.system("rm -f /data/*.jpg")
 while True:
   try:
     if myring.is_connected != True:
-      print("Existing Ring.com connection has timed out, reconnecting...")
+      print(datetime.datetime.now().replace(microsecond=0).isoformat() + ": Existing Ring.com connection has timed out, reconnecting...")
       myring = Ring(email, password)
     if myring.is_connected == True:
-      print("Ring.com connection is healthy.")
+      print(datetime.datetime.now().replace(microsecond=0).isoformat() + ": Ring.com connection is healthy.")
       d=0                                                                 
       for doorbells in myring.doorbells:                                  
         if isLatest(doorbells.last_recording_id) == 'true':               
-          print "Doorbell " + str(d) + ", recording " + str(doorbells.last_recording_id) + " was already saved."                                                                 
+          print(datetime.datetime.now().replace(microsecond=0).isoformat() + ": Doorbell " + str(d) + ", recording " + str(doorbells.last_recording_id) + " was already saved.")                                                               
         else:                                                                                                         
-          print("Getting recording URL for doorbell #" + str(d) + ". RecordingID: " + str(doorbells.last_recording_id) )
-          print(myring.is_connected)
+          print(datetime.datetime.now().replace(microsecond=0).isoformat() + ": Getting recording URL for doorbell #" + str(d) + ". RecordingID: " + str(doorbells.last_recording_id) )
           url=str(doorbells.recording_url(doorbells.last_recording_id))                                            
           if "https" not in url:
-            print("Invalid URL returned.  Exiting")
+            print(datetime.datetime.now().replace(microsecond=0).isoformat() + ": Invalid URL returned.  Exiting")
             sys.exit(1)
-          print(url)
-          print('Starting download for doorbell #' + str(d) + '. Recording: ' + str(doorbells.last_recording_id) )                                 
+          print(datetime.datetime.now().replace(microsecond=0).isoformat() + ': Starting download for doorbell #' + str(d) + '. Recording: ' + str(doorbells.last_recording_id) )                                 
           wgetCmd = 'wget -O /data/doorbell'+str(d)+'.mp4 -U "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2460.41 Safari/537.36" "'+url+'"' 
           os.system(wgetCmd)                                                                                                                        
-          print("Download complete, starting ffmpeg")                                                                                               
+          print(datetime.datetime.now().replace(microsecond=0).isoformat() + ": Download complete, starting ffmpeg")                                                                                               
           (                                                                                                                                         
             ffmpeg                                                                                                                                  
             .input("/data/doorbell"+str(d)+".mp4")                                                                                                  
@@ -55,23 +54,23 @@ while True:
             .output("/data/doorbell"+str(d)+"-%03d.jpg")                                                                                            
             .run()                                                                                                                                  
           )                                                                                                                                         
-          print("ffmpeg complete, starting jpeg optimization")                                                                                      
+          print(datetime.datetime.now().replace(microsecond=0).isoformat() + ": ffmpeg complete, starting jpeg optimization")                                                                                      
           jpgCmd = "jpegoptim -f -q -s /data/doorbell"+str(d)+"*.jpg"                                                                               
           os.system(jpgCmd)                                                                                                                         
-          print("jpeg optimization complete, creating gif")                                                                                         
+          print(datetime.datetime.now().replace(microsecond=0).isoformat() + ": jpeg optimization complete, creating gif")                                                                                         
           gifCmd = "convert -delay 20 -loop 0 /data/doorbell"+str(d)+"*.jpg -resize " + resolution + " /data/doorbell" + str(d) + ".gif"                 
           os.system(gifCmd)                                                               
-          print("gif created.  Saving index of converted video file")                                                                              
+          print(datetime.datetime.now().replace(microsecond=0).isoformat() + ": gif created.  Saving index of converted video file")                                                                              
           with open('device_tracking.db', mode='a') as db:                                                                                         
             dbo = csv.writer(db, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)                                                          
             dbo.writerow(['doorbell', d, str(doorbells.last_recording_id)])                                                                        
           clearJpgCmd = "rm -f /data/doorbell"+str(d)+"*.jpg"                                                                               
           os.system(clearJpgCmd)  
-          print("New GIF generated: doorbell" + str(d) + ".gif")                                                                                                                        
+          print(datetime.datetime.now().replace(microsecond=0).isoformat() + ": New GIF generated: doorbell" + str(d) + ".gif")                                                                                                                        
         d=d+1 
     time.sleep(5)
   except NameError:
-    print("Connecting to Ring.com.")
+    print(datetime.datetime.now().replace(microsecond=0).isoformat() + ": Connecting to Ring.com.")
     myring = Ring(email, password)
 
 
